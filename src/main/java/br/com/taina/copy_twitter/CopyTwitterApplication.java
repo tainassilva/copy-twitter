9cc71008818817b9030b0bed60d0c4f3124110a8
Taina SSantos Silva <tainassilva25111996@gmail.com>
2025-03-09 13:33:19 -0300

    Inicialzando o projeto spring.

---
+++ src/main/java/br/com/taina/copy_twitter/CopyTwitterApplication.java
@@ -0,0 +1,13 @@
+package br.com.taina.copy_twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopyTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopyTwitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/taina/copy_twitter/CopyTwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.taina.copy_twitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopyTwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
